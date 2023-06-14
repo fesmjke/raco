08e1a3d20b1fbe167a115159a625c1a4e944e1a5
fesmjke <fatalerror.sm@gmail.com>
2023-06-14 23:48:30 +0300

    feat: removed adjacent matrix and clean up solve method

--- src/brute_force.rs
+++ src/brute_force.rs
@@ -50,34 +50,13 @@ impl BruteForce {
 
 impl Solution for BruteForce {
     fn solve(&self, cities : &Vec<City>) -> Vec<Vec<City>> {
-        // Adjacency matrix
-        let mut matrix : Vec<Vec<f32>> = vec![];
-        
-        for i in 0..cities.len() {
-            
-            let mut temp : Vec<f32> = vec![];
-
-            for j in 0..cities.len() {
-
-                let distance = cities[i].position.distance_to(cities[j].position) as f32;
-
-                temp.push(distance);
-            }
-
-            matrix.push(temp);
-        }
-
         let mut root_route: Vec<City> = vec![];
 
         for city in cities.iter() {
             root_route.push(city.clone());
         }
-
-        let mut routes: Vec<Vec<City>> = vec![];
-
-        routes = self.permutations(&root_route);
-
-        return routes;
+        
+        self.permutations(&root_route)
     }
 }
 
