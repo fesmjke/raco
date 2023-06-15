7d5b43b39949c7ae079d97d3e2e678e7d5c7388e
fesmjke <fatalerror.sm@gmail.com>
2023-06-15 23:17:13 +0300

    feat: added method draw_mut when need's change self logic, and added states

--- src/drawable.rs
+++ src/drawable.rs
@@ -1,5 +1,11 @@
 use raylib::prelude::RaylibDraw;
 
+pub enum DrawState {
+    Drawing,
+    Finished
+}
+
 pub trait Drawable {
-    fn draw<T: RaylibDraw>(&self, d : &mut T);
+    fn draw<T: RaylibDraw>(&self, d : &mut T) {}
+    fn draw_mut<T: RaylibDraw>(&mut self, d : &mut T) -> DrawState { DrawState::Finished }
 }
