26d3a4d29aa891207d0a1c27cc3367bfc73dfae9
fesmjke <fatalerror.sm@gmail.com>
2023-06-16 23:31:24 +0300

    feat: added nn algorithm implementation

--- src/algorithms/nearest_neighbour.rs
+++ src/algorithms/nearest_neighbour.rs
@@ -9,11 +9,58 @@ impl NearestNeighbour {
 
         }
     }
+
+    fn calculate_distance(&self, from: &City, to: &City) -> f32 {
+        from.position().distance_to(*to.position())
+    }
+
+    fn select_nearest(&self, from : &City, left: &Vec<City>) -> usize {
+        let mut distance = self.calculate_distance(from, &left[0]);
+        let mut lowest = 0 as usize;
+
+        for (index,city) in left.iter().enumerate() {
+            let temp = self.calculate_distance(from, city);
+
+            if temp < distance {    
+                distance = temp;
+                lowest = index;
+            }
+        }
+
+        lowest
+    }
 }
 
 impl Solution for NearestNeighbour {
     fn solve(&self, cities : &Vec<City>) -> Vec<Vec<City>> {
-        todo!()
+        let mut routes : Vec<Vec<City>> = vec![];
+
+        let root_route = cities[1..].to_vec().clone();
+
+        for (index,city) in root_route.iter().enumerate() {
+            let mut temp = root_route.clone();
+            let mut route : Vec<City> = vec![];
+            let mut lowest = 0 as usize;
+            let mut start = city.clone();
+
+            route.push(temp[index].clone());
+
+            temp.remove(index);
+
+            while temp.len() > 0 {
+                lowest = self.select_nearest(&start, &temp);
+
+                route.push(temp[lowest].clone());
+
+                start = temp[lowest].clone();
+
+                temp.remove(lowest);
+            }
+
+            routes.push(route);
+        }
+
+        routes
     }
 }
 
@@ -34,5 +81,19 @@ mod solutions {
         let nn = NearestNeighbour::new();
 
         let mut answer = nn.solve(&cities);
+
+        assert_eq!(2, answer.len());
+
+        cities.push(city_d);
+
+        answer = nn.solve(&cities);
+
+        assert_eq!(3, answer.len());
+
+        cities.push(city_e);
+
+        answer = nn.solve(&cities);
+
+        assert_eq!(4, answer.len());
     }
 }
