f2581455dde1f245b5163fc5cc1c83a93c05ef26
fesmjke <fatalerror.sm@gmail.com>
2023-06-29 01:38:13 +0300

    feat: added prim's algorithm for finding minimum spanning tree

--- src/algorithms/christofides.rs
+++ src/algorithms/christofides.rs
@@ -1,9 +1,55 @@
 use crate::solution::Solution;
 use crate::city::City;
+use crate::utils::adjacency_matrix;
 
 pub struct Christofides;
 
-impl Christofides {
+type Pair = (usize, usize);
+
+fn prims(cities: &Vec<City>, root: usize) -> Vec<Pair> {
+    let matrix = adjacency_matrix(cities);
+    let mut edges : Vec<Pair> = vec![];
+
+    let mut visited = vec![root];
+
+    let mut lowest = f32::MAX;
+
+    let mut temp_col = root;
+    let mut temp_row = root;
+
+    while visited.len() != cities.len() {
+        for row in 0..matrix.len() {
+
+            if visited.contains(&row) {
+                continue;
+            }
+
+            for visit in visited.clone().into_iter() {
+
+                for col in 0..matrix[row].len() {
+                
+                    if col != visit {
+                        continue;
+                    }
+                    
+                    if lowest > matrix[row][visit] && matrix[row][visit] != 0.0 {
+                        lowest = matrix[row][visit];
+                        temp_col = row;
+                        temp_row = visit;
+                    }
+                }
+            }
+        }
+
+        lowest = f32::MAX;
+        edges.push((temp_row, temp_col));
+        visited.push(temp_col);
+    }
+    
+    edges
+}
+
+impl Christofides {    
     pub fn new() -> Self {
         Self {
 
@@ -23,6 +69,6 @@ mod solutions {
 
     #[test]
     fn christofides() {
-        
+
     }
 }
