7cb01a711089192f0478dd450268c6489bd2f9fc
fesmjke <fatalerror.sm@gmail.com>
2023-07-05 21:18:35 +0300

    refactor: removed states and draw_mut

--- src/drawable.rs
+++ src/drawable.rs
@@ -1,11 +1,5 @@
 use raylib::prelude::RaylibDraw;
 
-pub enum DrawState {
-    Drawing,
-    Finished
-}
-
 pub trait Drawable {
     fn draw<T: RaylibDraw>(&self, d : &mut T) {}
-    fn draw_mut<T: RaylibDraw>(&mut self, d : &mut T) -> DrawState { DrawState::Finished }
 }
