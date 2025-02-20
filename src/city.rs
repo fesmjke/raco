0774764d397f806f36de0be912013ad475fbda71
fesmjke <fatalerror.sm@gmail.com>
2025-02-20 14:42:16 +0000

    ref: updated methods, and draw method

--- src/city.rs
+++ src/city.rs
@@ -1,22 +1,28 @@
-use raylib::prelude::{RaylibDraw,Color,Vector2};
 use crate::drawable::Drawable;
+use macroquad::color::BLACK;
+use macroquad::math::Vec2;
+use macroquad::shapes::draw_circle;
 
-#[derive(Clone)]
+#[derive(Copy, Clone)]
 pub struct City {
-    position : Vector2,
-    size: f32, 
+    position: Vec2,
+    size: f32,
 }
 
 impl City {
-    pub fn new(x : f32, y: f32) -> Self {
+    pub fn new(x: f32, y: f32, size: f32) -> Self {
         Self {
-            position : Vector2 { x, y },
-            size: 5.0
+            position: Vec2 { x, y },
+            size,
         }
     }
 
-    pub fn position(&self) -> &Vector2 {
-        &self.position
+    pub fn position(&self) -> Vec2 {
+        self.position
+    }
+
+    pub fn distance_between(&self, city: &City) -> f32 {
+        self.position.distance(city.position())
     }
 }
 
@@ -27,7 +33,7 @@ impl PartialEq for City {
 }
 
 impl Drawable for City {
-    fn draw<T: RaylibDraw>(&self, d : &mut T) {
-        d.draw_circle_v(self.position, self.size, Color::BLACK);
+    fn draw(&self) {
+        draw_circle(self.position.x, self.position.y, self.size, BLACK);
     }
 }
