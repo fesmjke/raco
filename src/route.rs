42af2744d539111c64a27048e7d248ae5caf68a0
fesmjke <fatalerror.sm@gmail.com>
2025-02-20 14:30:22 +0000

    ref: renamed path to route, updated methods, updated draw method

--- src/route.rs
+++ src/route.rs
@@ -1,46 +1,25 @@
-use raylib::prelude::{RaylibDraw,Color};
-use crate::drawable::Drawable;
 use crate::city::City;
-use crate::utils::caclulate_distance;
+use crate::drawable::Drawable;
+use crate::utils::route_length;
 
-pub struct Path {
-    route : Vec<City>,
+pub struct Route<'a> {
+    pub(crate) cities: &'a Vec<City>,
 }
 
-impl Path {
-    pub fn new(cities: &Vec<City>) -> Self {
-        Self {
-            route : cities.to_vec() 
-        }
+impl<'a> Route<'a> {
+    pub fn new(cities: &'a Vec<City>) -> Self {
+        Self { cities: &cities }
     }
 
-    pub fn total_distance(&self) -> f32 {
-        caclulate_distance(&self.route)
+    pub fn path_length(&self) -> f32 {
+        route_length(&self)
     }
 
-    pub fn replace(&mut self, route: &Vec<City>) {
-        self.route = route.to_vec();
+    pub fn replace(&mut self, cities: &'a Vec<City>) {
+        self.cities = &cities;
     }
 }
 
-impl Drawable for Path {
-    fn draw<T: RaylibDraw>(&self, d : &mut T) {
-        let root = self.route.first().expect("missing root city in a route");
-
-        root.draw(d);
-
-        for (index,city) in self.route.iter().enumerate() {
-            city.draw(d);
-            d.draw_text(format!("{}", index).as_str(), city.position().x as i32, city.position().y as i32, 20, Color::RED);
-        }
-
-        let mut prev = root;
-
-        for city in self.route.iter() {
-            d.draw_line_v(prev.position(), city.position(), Color::BLACK);
-            prev = city;
-        }
-        
-        d.draw_line_v(prev.position(), root.position(), Color::BLACK);
-    }
+impl<'a> Drawable for Route<'a> {
+    fn draw(&self) {}
 }
