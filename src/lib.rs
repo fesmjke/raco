07b9eae3967ac3925bb49bce8b8f13d2dca208b9
fesmjke <fatalerror.sm@gmail.com>
2025-02-21 15:08:33 +0000

    ref: moved extra logic to lib, imported brute-force solution

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,31 @@
+use city::City;
 use route::Route;
 
 pub trait Rendereable {
     fn render(&self);
 }
 
-pub trait Solver {
-    fn solve(&self, routes: &Vec<Route>) -> Route;
+pub trait Solver<'a> {
+    /// solve - return best route
+    // TODO replace Route<'a> with Option<Route<'a>>
+    // due to some algorithms might have poor performance (Brute-Force)
+    fn solve(routes: &[Route]) -> Route;
+    fn get_routes(cities: &[City]) -> Vec<Route>;
 }
 
-pub mod algorithms;
+pub fn solve<'a, S>(routes: &'a Vec<Route>)
+where
+    S: Solver<'a>,
+{
+    S::solve(routes);
+}
+
+pub mod algorithms {
+    pub use brute_force::BruteForce;
+    pub mod brute_force;
+    // pub mod nearest_neighbour;
+}
+
 pub mod city;
 pub mod preset;
 pub mod route;
