94d3c2233285285c82bd86772a6a38393b9c506c
fesmjke <fatalerror.sm@gmail.com>
2025-02-24 15:10:40 +0000

    feat: implemented render for route

--- src/route.rs
+++ src/route.rs
@@ -1,3 +1,6 @@
+use macroquad::color::BLACK;
+use macroquad::shapes::draw_line;
+
 use crate::city::City;
 use crate::utils::route_length;
 use crate::Rendereable;
@@ -22,5 +25,23 @@ impl Route {
 }
 
 impl Rendereable for Route {
-    fn render(&self) {}
+    fn render(&self) {
+        self.cities
+            .iter()
+            .zip(self.cities.iter().cycle().skip(1))
+            .take(self.cities.len())
+            .for_each(|(xf, xt)| {
+                // render city
+                xf.render();
+                // render between
+                draw_line(
+                    xf.position().x,
+                    xf.position().y,
+                    xt.position().x,
+                    xt.position().y,
+                    1.0,
+                    BLACK,
+                );
+            });
+    }
 }
